02acd0987cf53808d89f4bb9306e816d5b60946e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-08 08:34:57 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -66,7 +66,8 @@ class  AspectRatioButton(ToolButton):
 # Define class AspectRatioScript.
 class AspectRatioScript(scripts.Script):
     '''Class for selecting the aspect ratio.'''
-    def __init__(self, ar=1.0, **kwargs):
+    #def __init__(self, ar=1.0, **kwargs):
+        #
         #self.ar_values_0 = (1.0, 2.0, 3/2, 4/3, 5/3, 5/4, 6/5,
         #                    7/5, 14/9, 15/9, 16/9, 16/10)
         #self.ar_values_1 = (1.0, 0.5, 2/3, 3/4, 3/5, 4/5, 5/6,
