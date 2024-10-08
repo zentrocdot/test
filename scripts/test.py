0a85fd063bbeae8841870a6536c455e173469fbe
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-08 17:20:10 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -45,8 +45,8 @@ class  AspectRatioButton(ToolButton):
         # Initialise height and width.
         w = _width
         h = _height
-        #print(ar)
-        #print(w, h)
+        print(self.ar)
+        print(w, h)
         # Calculate new width and height.
         if self.ar > 1.0:  # fixed height, change width
             w = self.ar * h
