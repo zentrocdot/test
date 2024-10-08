0404bc35ae931585e9af79c5cd33a61713d146d1
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-08 14:08:44 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -91,7 +91,7 @@ class AspectRatioScript(scripts.Script):
                 else:
                     resolution = [self.t2i_w, self.t2i_h]
                 use_prompt_button.click(
-                        [1.0, "1:1"],
+                        use_prompt_button.apply,
                         inputs=resolution,
                         outputs=resolution
                 )
