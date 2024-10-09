76609d0f44ecdeb54135478ee94ff5944d11b1b9
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-09 17:26:39 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -45,7 +45,8 @@ class AspectRatioScript(scripts.Script):
                 False, label="Aspect Ratio Calculator", 
                 elem_id=f'{"img" if is_img2img else "txt"}2img_row_aspect_ratio'
             ) as enabled:
-                arvalue = gr.Number(label="Calculated aspect ratio from Width/Height", value=-1, interactive=False, render=True)
+                #arvalue = gr.Number(label="Calculated aspect ratio from Width/Height", value=-1, interactive=False, render=True)
+                arvalue = gr.Textbox(label="Calculated aspect ratio from Width/Height", value=-1, interactive=False, render=True)
                 with gr.Row(
                     elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
                 ):
