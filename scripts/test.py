548382bc05cf6d7233acec657f6cadb799344bd3
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-09 16:03:09 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -146,7 +146,7 @@ class AspectRatioScript(scripts.Script):
                     )
                     def lala(x,y):
                         print("x=", x, "y=", y)   
-                        ret = str(x) + " x " + str(y)      
+                        ret = x/y      
                         #btn.prtval      
                         #print("START")      
                         #print(self.t2i_w, self.t2i_h)
