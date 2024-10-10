d532c1a584956583b790814dc6bd707a83399742
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-10 09:28:09 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -20,9 +20,10 @@ from modules.ui_components import InputAccordion
 _prec = 2
 
 def update_prec(prec):
-    global _prec
-    _prec = int(prec)
-    return prec
+    #global _prec
+    #_prec = int(prec)
+    #return _prec
+    return int(prec)
 
 # Function update_number()
 def update_number(x,y):
@@ -92,7 +93,7 @@ class AspectRatioScript(scripts.Script):
                     mybutton = gr.Button("Calculate Aspect Ratio")
                     with contextlib.suppress(AttributeError):
                         #imgres = self.image_resolution(is_img2img)
-                        prec.input(update_prec, inputs=[prec], outputs=[prec])
+                        prec.input(update_prec, inputs=[prec], outputs=[_prec])
                         mybutton.click(update_number, inputs=[wentry, hentry], outputs=arvalue)
 
     # Class method after_component.
