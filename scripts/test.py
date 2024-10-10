4448e14cb0dc457be116a5501eb8ce91b5f711c2
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-10 03:43:39 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -3,39 +3,38 @@ Extension for AUTOMATIC1111.
 
 Version 0.0.0.1
 '''
-# pylint: disable=invalid-name
-# pylint: disable=too-few-public-methods
-# pylint: disable=attribute-defined-outside-init
 # pylint: disable=import-error
 # pylint: disable=consider-using-from-import
-# pylint: disable=trailing-whitespace
-# pylint: disable=unused-argument
-# pylint: disable=too-many-instance-attributes
 # pylint: disable=no-self-use
+# pylint: disable=invalid-name
+# pylint: disable=unused-variable
+# pylint: disable=unused-argument
+# pylint: disable=attribute-defined-outside-init
 
 # Import the Python modules.
 import contextlib
+from modules.ui_components import InputAccordion
 import gradio as gr
 import modules.scripts as scripts
-from modules.ui_components import InputAccordion
 
 def update_number(x,y):
+    '''Helper function update number.'''
     if x > y:
         z = x/y
         if float(z).is_integer():
-            z = int(z)                                 
-        ret = str(z) + ":1"                   
+            z = int(z)
+        ret = str(z) + ":1"
     elif x <= y:
         z = y/x
         if float(z).is_integer():
             z = int(z)
-        ret = "1:" + str(z)    
+        ret = "1:" + str(z)
     return str(ret)
 
 # Define class AspectRatioScript.
 class AspectRatioScript(scripts.Script):
     '''Class for calculating the aspect ratio.'''
-    
+
     def title(self):
         '''Class method title.'''
         return "Aspect Ratio Calculator"
@@ -45,6 +44,7 @@ class AspectRatioScript(scripts.Script):
         return scripts.AlwaysVisible  # Hide this script in the Scripts dropdown
 
     def image_resolution(self, is_img2img):
+        '''Get the image resolution.'''
         if is_img2img:
             imgres = [self.i2i_w, self.i2i_h]
         else:
@@ -57,32 +57,33 @@ class AspectRatioScript(scripts.Script):
         with gr.Column(
             elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
         ):
-            # Create an accordion. 
+            # Create an accordion.
             with InputAccordion(
-                False, label="Aspect Ratio Calculator", 
+                False, label="Aspect Ratio Calculator",
                 elem_id=f'{"img" if is_img2img else "txt"}2img_row_aspect_ratio'
             ) as enabled:
-                arvalue = gr.Textbox(value=update_number(512,512), lines=1, interactive=False, inputs=None,
+                arvalue = gr.Textbox(value=update_number(512,512), lines=1,
+                    interactive=False, inputs=None,
                     label="Calculated aspect ratio from Width/Height"
                 )
                 # Create a row.
                 with gr.Row(
                     elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
                 ):
-                    # Create two numeric fields and one button. 
+                    # Create two numeric fields and one button.
                     wentry = gr.Number(label="Width", interactive=True, precision=0)
                     hentry = gr.Number(label="Height", interactive=True, precision=0)
-                    mybutton = gr.Button("Calculate Aspect Ratio")          
+                    mybutton = gr.Button("Calculate Aspect Ratio")
                     with contextlib.suppress(AttributeError):
                         imgres = self.image_resolution(is_img2img)
-                        mybutton.click(update_number, inputs=[wentry, hentry], outputs=arvalue)               
-          
+                        mybutton.click(update_number, inputs=[wentry, hentry], outputs=arvalue)
+
     # Class method after_component.
     def after_component(self, component, **kwargs):
         '''Class method after_component.
 
         This method is used to generalize the existing programme code. It is
-        detected if one is in the 'txt2img' tab or in the 'img2img' tab in the 
+        detected if one is in the 'txt2img' tab or in the 'img2img' tab in the
         web UI. Then the corresponding self variables can be used in the same
         programme code for both tabs.
         '''
