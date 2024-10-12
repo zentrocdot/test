05dfaf0c5bb3997d0878b704cac08d63bd10cb40
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-12 17:29:49 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -116,7 +116,7 @@ class AspectRatioScript(scripts.Script):
                             interactive=True, label="Calculation of Width/Height")
                     hy = gr.Number(value=0, render=True, visible=True,
                             interactive=True, label="Calculation of Width/Height")
-                    rb_on_off = gr.Radio(["On/Off"], label="Exact Calculation")    
+                    rb_on_off = gr.Radio(["On", "Off"], label="Exact Calculation")    
                 with gr.Row(elem_id=css_row):
                     ar_input = gr.Textbox(value="1:1", render=True, label="Aspect Ratio")     
                     calc_btn = gr.Button(label="Calculate")
