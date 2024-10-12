0639e018653af6a3b3928de117e8dd1933069a87
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-12 12:26:19 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,108 +1,150 @@
-'''sd-webui-aspect_ratio_calc
+'''sd-webui-uncommon_aspect_ratios
 Extension for AUTOMATIC1111.
 
-Version 0.0.0.1
+Version 0.0.0.2
 '''
+# pylint: disable=invalid-name
+# pylint: disable=too-few-public-methods
+# pylint: disable=attribute-defined-outside-init
 # pylint: disable=import-error
 # pylint: disable=consider-using-from-import
-# pylint: disable=no-self-use
-# pylint: disable=invalid-name
-# pylint: disable=unused-variable
+# pylint: disable=trailing-whitespace
 # pylint: disable=unused-argument
-# pylint: disable=attribute-defined-outside-init
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=no-self-use
+# pylint: disable=bad-indentation
+# pylint: disable=unused-variable
 
 # Import the Python modules.
+import contextlib
 import gradio as gr
 import modules.scripts as scripts
-import contextlib
-from modules.ui_components import InputAccordion
+from modules.ui_components import ToolButton, InputAccordion
 
-_prec = 2
+# Define module variables.
+_width = 512
+_height = 512
 
-def update_prec(prec):
-    global _prec
-    _prec = int(prec)
-    return prec
+# Define the module aspect ratio dictionary.
+ardict = {"1:1    ": 1.0,     "1.5:1  ": 1.5,     "2:1   ": 2.0,     "2.4:1": 2.4, 
+          "3:1    ": 3.0,     "3:2    ": 3/2,     "3.2:1 ": 3.2,     "3.6:1 ": 3.6,
+          "4:1    ": 4.0,     "4:3    ": 4/3,     "5:1   ": 5.0,     "5:3   ": 5/3,
+          "5:4    ": 5/4,     "6:1    ": 6.0,     "6:5   ": 6/5,     "7:1   ": 7.0, 
+          "7:4    ": 7/4,     "7:5    ": 7/5,     "7:5.5 ": 7/5.5,   "8:5   ": 8/5,   
+          "9:16   ": 9/16,    "10:12  ": 10/12,   "11:5  ": 11/5,    "12:5  ": 12/5,
+          "13:18  ": 13/18,   "13:19  ": 13/19,   "14:9  ": 14/9,    "15:9  ": 15/9,  
+          "16:9   ": 16/9,    "16:10  ": 16/10,   "17:22 ": 17/22,   "18:5  ": 18/5,  
+          "18:9   ": 18/9,    "18.5:9 ": 18.5/9,  "19.5:9": 19.5/9,  "20:9  ": 20/9, 
+          "21:9   ": 21/9,    "22:9   ": 22/9,    "32:9  ": 32/9,    "36:10 ": 3.6,
+          "45:35  ": 45/35,   "55:23  ": 55/23,   "64:27": 64/27,    "69:25 ": 69/25,
+          "239:100": 239/100, "256:135": 256/135, "1.19:1 ": 1.19,   "1.25:1": 1.25,
+          "1.3:1 ": 1.3,      "1.33:1 ": 1.33,    "1.37:1 ": 1.37,   "1.375:1": 1.375,
+          "1.40:1": 1.40,     "1.41:1 ": 1.41,    "1.43:1 ": 1.43,   "1.54:1 ": 1.54,
+          "1.59:1": 1.59,     "1.6:1  ": 1.6,     "1.618:1": 1.618,  "1.66:1 ": 1.66,
+          "1.75:1 ": 1.75,    "1.77:1 ": 1.77,    "1.78:1": 1.78,    "1.85:1 ": 1.85,
+          "1.875:1": 1.875,   "2.125 ": 2.125,    "2.16:1": 2.16,    "2.20:1 ": 2.20,
+          "2.21:1 ": 2.21,    "2.35:1": 2.35,     "2.37:1": 2.37,    "2.38:1 ": 2.38,
+          "2.39:1 ": 2.39,    "2.40:1": 2.40,     "2.66:1": 2.66,    "2.75:1 ": 2.75, 
+          "2.76:1 ": 2.76,    "3.55:1": 3.55,     "3.58:1": 3.58}
 
-# Function update_number()
-def update_number(x,y):
-    '''Helper function update number.'''
-    if x > y:
-        z = x/y
-        z = round(z, _prec)
-        if float(z).is_integer():
-            z = int(z)
-        ret = str(z) + ":1"
-    elif x <= y:
-        z = y/x
-        z = round(z, _prec)
-        if float(z).is_integer():
-            z = int(z)
-        ret = "1:" + str(z)
-    return str(ret)
+# Declare the aspect ratio list.
+arlist = []
+
+# Create the aspect ratio list.
+for key, value in ardict.items():
+    arlist.append(key)  
+
+# Define class AspectRatioButton.
+class  AspectRatioButton(ToolButton):
+    '''Class for calculating the new Width and new Height for
+       use in the web UI from the chosen aspect ratio.
+    '''
+    def __init__(self, ar=1.0, **kwargs):
+        '''Class init method.'''
+        super().__init__(**kwargs)
+        self.ar = ar
+
+    def apply(self, w, h):
+        '''Class method apply.'''
+        # Initialise height and width.
+        w = _width
+        h = _height
+        # Calculate new width and height.
+        if self.ar > 1.0:  # fixed height, change width
+            w = self.ar * h
+        elif self.ar < 1.0:  # fixed width, change height
+            h = w / self.ar
+        else:  # set minimum dimension to both
+            min_dim = min([w, h])
+            w, h = min_dim, min_dim
+        # Create a new list.
+        retlst = list(map(round, [w, h]))
+        # Return the list with width and height.
+        return retlst
 
 # Define class AspectRatioScript.
 class AspectRatioScript(scripts.Script):
-    '''Class for calculating the aspect ratio.'''
-
+    '''Class for selecting the aspect ratio.'''
+    
     def title(self):
         '''Class method title.'''
-        return "Aspect Ratio Calculator"
+        return "Aspect Ratio Selector"
 
     def show(self, is_img2img):
         '''Class method show.'''
-        return scripts.AlwaysVisible  # Hide this script in the Scripts dropdown
+        return scripts.AlwaysVisible  # hide this script in the Scripts dropdown
 
     def image_resolution(self, is_img2img):
-        '''Get the image resolution.'''
+        '''Get the image resolution from container and return the values.'''
         if is_img2img:
             imgres = [self.i2i_w, self.i2i_h]
         else:
             imgres = [self.t2i_w, self.t2i_h]
-        return imgres
+        return imgres    
 
     def ui(self, is_img2img):
         '''Class method ui.'''
-        # Create a column.
-        with gr.Column(
-            elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
-        ):
-            # Create an InputAccordion.
-            with InputAccordion(value="",
-                label="Aspect Ratio Calculator",
-                elem_id=f'{"img" if is_img2img else "txt"}2img_row_aspect_ratio',
-                open=False
+        # Set the css format strings.
+        css_acc = f'{"img" if is_img2img else "txt"}2img_accordion_aspect_ratio' 
+        css_col = f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
+        css_row = f'{"img" if is_img2img else "txt"}2img_row_aspect_ratio'
+        # Loop over the columns.
+        with gr.Column(elem_id=css_col):
+            with InputAccordion(value=False,
+                label="Common Landscape Aspect Ratios", 
+                elem_id=css_acc
             ) as enabled:
-                # Create a row.
-                with gr.Row(
-                    elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
-                ):   
-                    arvalue = gr.Textbox(value=update_number(512,512), lines=1,
-                        interactive=False, inputs=None,
-                        label="Calculated aspect ratio from Width/Height"
-                    )
-                    prec = gr.Dropdown([0,1,2,3,4,5,6,7,8], label="Precision", value="2")          
-                # Create a row.
-                with gr.Row(
-                    elem_id=f'{"img" if is_img2img else "txt"}2img_container_aspect_ratio'
-                ):
-                    # Create two numeric fields and one button.
-                    wentry = gr.Number(label="Width", interactive=True)
-                    hentry = gr.Number(label="Height", interactive=True)
-                    mybutton = gr.Button("Calculate Aspect Ratio")
+                arval = gr.Dropdown(arlist, label="Aspect Ratios", value="1:1")
+                with gr.Row(elem_id=css_row):
+                    rst = AspectRatioButton(ar=1.0, value="Reset")
+                    btn = AspectRatioButton(ar=1.0, value="Apply")
+                    chg = AspectRatioButton(ar=1.0, value="Change Orientation")
                     with contextlib.suppress(AttributeError):
-                        #imgres = self.image_resolution(is_img2img)
-                        prec.input(update_prec, inputs=[prec], outputs=[prec])
-                        mybutton.click(update_number, inputs=[wentry, hentry], outputs=arvalue)
-
+                        imgres = self.image_resolution(is_img2img)
+                        def update_button(arstr):
+                            btn.ar = ardict[arstr]
+                            return btn.apply(_width, _height)
+                        btn.click(update_button, inputs=[arval], outputs=imgres)
+                        def update_rst0(arstr): 
+                            rst.ar = 1.0
+                            return rst.apply(_width, _height)
+                        def update_rst1(arstr): 
+                            rst = "1:1"
+                            return rst
+                        rst.click(update_rst0, inputs=[arval], outputs=imgres)
+                        rst.click(update_rst1, inputs=[arval], outputs=[arval])
+                        def update_chg(arstr):
+                            chg.ar = 1/ardict[arstr]
+                            return chg.apply(_width, _height)
+                        chg.click(update_chg, inputs=[arval], outputs=imgres
+                    )
     # Class method after_component.
     def after_component(self, component, **kwargs):
         '''Class method after_component.
 
-        This method is used to generalize the existing programme code. It is
-        detected if one is in the 'txt2img' tab or in the 'img2img' tab in the
-        web UI. Then the corresponding self variables can be used in the same
-        programme code for both tabs.
+        This method is used to generalize the existing code. It is detected if 
+        one is in the txt2img tab or the img2img tab. Then the corresponding self
+        variables can be used in the same code for both tabs.
         '''
         if kwargs.get("elem_id") == "txt2img_width":
             self.t2i_w = component
