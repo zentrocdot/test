6411a19c2362d966177b81617c4e4ab03519c6cb
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-12 19:21:56 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -110,7 +110,7 @@ class ResolutionCalcScript(scripts.Script):
                             interactive=False, info="Height")
                     rb_on_off = gr.Radio(choices=["On", "Off"], value="Off", label="Exact Calculation")    
                 with gr.Row(elem_id=css_row):
-                    arcalc_input = gr.Textbox(value="", info="Aspect Ratio", label="", placeholder="Enter the aspect ratio here")     
+                    arcalc_input = gr.Textbox(value="", info="Aspect Ratio", label="", placeholder="Enter aspect ratio here")     
                     calc_btn = gr.Button(value="Calculate")
                     adopt_btn = gr.Button(value="Adopt")
                     with contextlib.suppress(AttributeError):
