38f2e5b1e7f3bc4b1d3036b821aa862a7ff70e5c
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-13 09:10:32 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -114,7 +114,7 @@ class ResolutionCalcScript(scripts.Script):
                     rb_on_off = gr.Radio(choices=["On", "Off"], value="Off", label="Exact Calculation",
                                          info="of Width/Height", key="RB1", elem_id=css_radio, min_width=30)
                     round_on_off = gr.Radio(choices=["On", "Off"], value="Off", label="Rounding",
-                                            info="of Width/Height", key="RB2", elem_id=css_radio)
+                                            info="of Width/Height", key="RB2", elem_id=css_radio, scale=2)
                     with contextlib.suppress(AttributeError):
                         def change_rb(rb_state):
                             global _IsExact
