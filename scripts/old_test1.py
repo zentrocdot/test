9aaa86b2b9a41a439a548539b6c528a37953d60b
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-16 12:51:45 +0200

    Update old_test1.py

--- scripts/old_test1.py
+++ scripts/old_test1.py
@@ -26,34 +26,6 @@ import os
 
 lora_path = getattr(modules.shared.cmd_opts, "lora_dir", os.path.join(models.paths.models_path, "Lora"))
 
-lora_list = []
-
-def get_loras():
-    global lora_list
-    if not os.path.isdir(lora_path):
-        print("No valid path")
-        return []
-    def get_recursive(path: str):
-        out_list = []
-        global_path = os.path.join(lora_path, path)
-        for item in os.listdir(global_path):
-            if os.path.isfile(os.path.join(global_path, item)):
-                out_list.append(os.path.join(path, item))
-            elif os.path.isdir(os.path.join(global_path, item)):
-                out_list.extend(get_recursive(os.path.join(path, item)))
-        return out_list
-    lora_list = get_recursive("")
-
-#get_loras()
-
-print(lora_list)
-
-def lora_lists():
-    global lora_list
-    #if len(lora_list) == 0:
-    get_loras()
-    return lora_list
-
 def fast_lora_scan(dir, ext):  # dir: str, ext: list
     subfolders, files = [], []
     for f in os.scandir(dir):
@@ -66,9 +38,11 @@ def fast_lora_scan(dir, ext):  # dir: str, ext: list
         sf, f = run_fast_scandir(dir, ext)
         subfolders.extend(sf)
         files.extend(f)
-    #files = sorted(files, key=lambda x: x.count, reverse=True)
-    files.sort(reverse=True)
+    files.sort(reverse=False)
     return files
+
+def get_lora_list():
+    return fast_lora_scan(lora_path, [".safetensors"])
 
 def on_ui_tabs():
     # Create a new block.
@@ -76,7 +50,7 @@ def on_ui_tabs():
         # Create a new row. 
         with gr.Row():
                 input_file = gr.Dropdown(fast_lora_scan(lora_path, [".safetensors"]), label="Lora")
-                create_refresh_button(input_file, fast_lora_scan(lora_path, [".safetensors"]),
+                create_refresh_button(input_file, get_lora_list,
                                       lambda: {"choices": fast_lora_scan(lora_path, [".safetensors"])},
                                       "metadata_utils_refresh_1")
         with gr.Row():
